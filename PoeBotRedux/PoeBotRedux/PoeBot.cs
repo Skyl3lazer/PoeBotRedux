f91b92ec79897d15659fd2cd87ece357a51db96f
Skyl3lazer <derekjjjj@gmail.com>
2017-02-18 11:17:15 -0500

    Added ChatSharp Library from https://github.com/SirCmpwn/ChatSharp
    Added basic DB fetches

--- PoeBotRedux/PoeBotRedux/PoeBot.cs
+++ PoeBotRedux/PoeBotRedux/PoeBot.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
 
 namespace PoeBotRedux
 {
-    class Program
+    class PoeBot
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ PoeBotRedux/PoeBotRedux/Types/SystemSettings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoeBotRedux
+{
+    class SystemSettings
+    {
+        private data_System_Parameters systemParmsDataSet;
+        private data_System_ParametersTableAdapters.System_ParametersTableAdapter systemParmsAdapter;
+
+        public SystemSettings()
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            systemParmsDataSet = new data_System_Parameters();
+            systemParmsAdapter = new data_System_ParametersTableAdapters.System_ParametersTableAdapter();
+
+            systemParmsAdapter.Fill(systemParmsDataSet.System_Parameters);
+        }
+        public string getServerName()
+        {
+            return systemParmsAdapter.GetServerName();
+        }
+        public string getUserName()
+        {
+            return systemParmsAdapter.GetUserName();
+        }
+        public string getUserPassword()
+        {
+            return systemParmsAdapter.GetUserPassword();
+        }
+        public string getChannelName()
+        {
+            return systemParmsAdapter.GetChannelName();
+        }
+        public string getNick()
+        {
+            return systemParmsAdapter.GetNick();
+        }
+        public int getPort()
+        {
+            try
+            {
+                return int.Parse(systemParmsAdapter.GetPort());
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Could not retreive server port", e);
+            }
+        }
+    }
+}
