bf9edeae21f272b7038057c8bdb6d9b7879fb116
Skyl3lazer <derekjjjj@gmail.com>
2017-02-18 11:30:11 -0500

    Basic connection

--- PoeBotRedux/PoeBotRedux/PoeBot.cs
+++ PoeBotRedux/PoeBotRedux/PoeBot.cs
@@ -7,14 +7,44 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Threading;
+using ChatSharp;
 
 namespace PoeBotRedux
 {
     class PoeBot
     {
+        private static SystemSettings systemSettings;
+        private static IrcClient client;
+
         static void Main(string[] args)
         {
+            systemSettings = new SystemSettings();
 
+            Client();
+            Listen();
+
+            client.ConnectAsync();
+
+            while (true);
+        }
+        private static bool Client()
+        {
+            client = new IrcClient(systemSettings.getServerName(), new IrcUser(systemSettings.getNick(), systemSettings.getUserName()));
+
+            client.ConnectionComplete += (s, e) => client.JoinChannel(systemSettings.getChannelName());
+
+            return true;
+        }
+        private static bool Listen()
+        {
+            client.ChannelMessageRecieved += (s, e) =>
+            {
+                var channel = client.Channels[e.PrivateMessage.Source];
+
+                if (e.PrivateMessage.Message == ".list")
+                    channel.SendMessage(string.Join(", ", channel.Users.Select(u => u.Nick)));
+            };
+            return true;
         }
     }
 }
